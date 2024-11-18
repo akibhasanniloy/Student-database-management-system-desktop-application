d53613f0b4925e7b5363d0dca3b7506799e81fa2
Akib Hasan Niloy <akibhasanniloy2@gmail.com>
2024-11-18 18:19:59 +0600

    Update project.sql

--- project.sql
+++ project.sql
@@ -15,10 +15,7 @@ Department varchar(50),
 Intake varchar(40),
 Address varchar(90),
 Email varchar(40),
-Roll_No int,
-Phone_No int,
-Admin_id int,
-FOREIGN KEY (admin_id) REFERENCES admin(admin_id)
+Phone_No int
 );
 
 ALTER TABLE Student
@@ -26,23 +23,10 @@ DROP COLUMN Admin_id;
 
 ALTER TABLE Student DROP FOREIGN KEY Admin_id;
 
-ALTER TABLE Student
-DROP FOREIGN KEY Admin_id;
-
-
-ALTER TABLE Student 
-ADD COLUMN `Date` VARCHAR(50), 
-ADD COLUMN `Time` VARCHAR(50);
-
 alter table Student drop column Admin_id;
 
 ALTER TABLE Student DROP FOREIGN KEY student_ibfk_1;
 ALTER TABLE Student DROP COLUMN Admin_id;
-
-
-ALTER TABLE Student
-DROP COLUMN `Date`,
-DROP COLUMN `Time`;
 
 SELECT * FROM Student;
 
@@ -55,5 +39,3 @@ insert into admin values(1,"Akib","akib@gmail.com");
 insert into admin values(4,"Hasn","haan@gmail.com");
 
 INSERT INTO admin (Admin_id, Name, Email) VALUES (1, "Akib", "akib@gmail.com");
-
-DELETE FROM Student WHERE std_Id = '2';
